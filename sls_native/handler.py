5a8d54b09fc7199c628eb93f4e2ace12df1989dc
Alexander Uralov <yralov87@gmail.com>
2018-08-28 13:43:58 +0300

    native sls python app was finished

--- sls_native/handler.py
+++ sls_native/handler.py
@@ -1,15 +1,27 @@
 import json
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2.extras import DictCursor
+
+load_dotenv()
+
+connection = psycopg2.connect(
+    host=os.getenv('host'),
+    database=os.getenv('database'),
+    user=os.getenv('user'),
+    password=os.getenv('password'),
+)
+cursor = connection.cursor(cursor_factory=DictCursor)
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
+    cursor.execute("SELECT * FROM book_book;")
+    book = cursor.fetchone()
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(book)
     }
 
     return response
